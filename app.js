38a3849c66e80729c16abe4b87099676996b63f0
jotoca1975 <rindetetiratedaespacio@gmail.com>
2026-09-17 19:03:28 +0200

    Initialize Supabase client and fetch travelers data
    Added Supabase client initialization and data fetching function.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 
+const supabaseUrl = "https://xiarahccciclooxagezc.supabase.co";
+const supabaseKey = "sb_publishable_eobhXEKZnbdRgmuE42tKDA_mk34Dgh0";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+async function cargarViajeros() {
+
+    const { data, error } = await supabase
+        .from("viajeros")
+        .select("*");
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    console.log("Conectado correctamente.");
+    console.table(data);
+
+}
+
+cargarViajeros();
